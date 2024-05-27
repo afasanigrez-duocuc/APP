ec9bcc5b1d0d2698c22b6ba273cc6403e3803f4b
afasanigrez-duocuc <ar.fasani@duocuc.cl>
2024-05-26 23:32:07 -0400

    Semana 2

--- MyAPP/src/app/home/home.page.ts
+++ MyAPP/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PickerController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,48 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  nombre: string = 'Juan';
-  nombres: string[] = ['Juan', 'Pedro', 'Luis'];
+  username: string = '';
+  nombre: string = '';
+  apellido: string = '';
+  educacion: string = '';
+  nacimiento: string = '';
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute, 
+    private pickerCtrl: PickerController,
+    private toastCtrl: ToastController
+  ) {
+    this.route.queryParams.subscribe(params => {
+      this.username = params['username'];
+    });
+  }
 
+  limpiarCampos() {
+    this.nombre = '';
+    this.apellido = '';
+    this.educacion = '';
+    this.nacimiento = '';
+    
+    const nombreInput = document.querySelector('#nombreInput');
+    const apellidoInput = document.querySelector('#apellidoInput');
+
+    if (nombreInput && apellidoInput) {
+      nombreInput.classList.add('shake');
+      apellidoInput.classList.add('shake');
+
+      setTimeout(() => {
+        nombreInput.classList.remove('shake');
+        apellidoInput.classList.remove('shake');
+      }, 1000); // Duración de la animación
+    }
+  }
+
+  async mostrarInformacion() {
+    const toast = await this.toastCtrl.create({
+      message: `Nombre: ${this.nombre}, Apellido: ${this.apellido}`,
+      duration: 3000,
+      position: 'bottom'
+    });
+    await toast.present();
+  }
 }
--- MyAPP/src/app/registro/registro.page.ts
+++ MyAPP/src/app/registro/registro.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
-export class RegistroPage implements OnInit {
 
-  constructor() { }
+export class RegistroPage {
 
-  ngOnInit() {
+  username: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
+  constructor(private router: Router, private navCtrl: NavController) {}
+
+  login() {
+    if (this.username && this.password && this.username.length >= 3 && this.username.length <= 8 && /^\d{4}$/.test(this.password)) {
+      this.router.navigate(['/home'], { queryParams: { username: this.username } });
+      this.navCtrl.navigateRoot(['/home']);
+    } else {
+      alert('Por favor, ingrese un usuario alfanumérico de 3 a 8 caracteres y una contraseña numérica de 4 dígitos.');
+    }
   }
-
 }
