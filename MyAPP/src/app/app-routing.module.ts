044b30c62c8c3aa59985b78f7f23555e360898ea
afasanigrez-duocuc <ar.fasani@duocuc.cl>
2024-06-02 16:45:32 -0400

    Página de actualización de datos de usuario.

--- MyAPP/src/app/app-routing.module.ts
+++ MyAPP/src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
     path: 'registro',
     loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
   },
+  {
+    path: 'datos',
+    loadChildren: () => import('./datos/datos.module').then( m => m.DatosPageModule)
+  },
 ];
 
 @NgModule({
--- MyAPP/src/app/datos/datos.page.ts
+++ MyAPP/src/app/datos/datos.page.ts
@@ -5,9 +5,9 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: 'app-datos',
+  templateUrl: 'datos.page.html',
+  styleUrls: ['datos.page.scss'],
   animations: [
     trigger('shake', [
       state('start', style({ transform: 'translateX(0)' })),
@@ -23,7 +23,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   ]
 })
 
-export class HomePage {
+export class DatosPage {
 
   username: string = '';
   nombre: string = '';
@@ -40,5 +40,34 @@ export class HomePage {
     this.route.queryParams.subscribe(params => {
       this.username = params['username'];
     });
+  }
+
+  limpiarCampos() {
+    this.nombre = '';
+    this.apellido = '';
+    this.educacion = '';
+    this.nacimiento = '';
+
+    const nombreInput = document.querySelector('#nombreInput');
+    const apellidoInput = document.querySelector('#apellidoInput');
+
+    if (nombreInput && apellidoInput) {
+      nombreInput.classList.add('shake');
+      apellidoInput.classList.add('shake');
+
+      setTimeout(() => {
+        nombreInput.classList.remove('shake');
+        apellidoInput.classList.remove('shake');
+      }, 1000);
+    }
+  }
+
+  async mostrarInformacion() {
+    const toast = await this.toastCtrl.create({
+      message: `Nombre: ${this.nombre}, Apellido: ${this.apellido}`,
+      duration: 3000,
+      position: 'bottom'
+    });
+    await toast.present();
   }
 }
